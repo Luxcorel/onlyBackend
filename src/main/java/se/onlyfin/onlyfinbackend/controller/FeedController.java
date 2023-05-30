96e7d1b2e6f848734ace349e0e972156619949f1
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-05-31 01:44:07 +0200

    Update FeedController.java

--- src/main/java/se/onlyfin/onlyfinbackend/controller/FeedController.java
+++ src/main/java/se/onlyfin/onlyfinbackend/controller/FeedController.java
@@ -57,8 +57,9 @@ public class FeedController {
      * @return a list of feed cards
      */
     @GetMapping("/all-the-things")
-    public ResponseEntity<List<FeedCardDTO>> fetchFeedAll(Principal principal, @Nullable ZoneId zoneId) {
+    public ResponseEntity<List<FeedCardDTO>> fetchFeedAll(Principal principal, @Nullable TimeZone timeZone) {
         User fetchingUser = userService.getUserOrException(principal.getName());
+        TimeZone timeZoneToUse = Objects.requireNonNullElse(timeZone, TimeZone.getDefault());
 
         List<Subscription> subscriptions = subscriptionRepository.findBySubscriber(fetchingUser);
         if (subscriptions.isEmpty()) {
@@ -73,7 +74,7 @@ public class FeedController {
             analystUsernameToIdMap.put(currentAnalyst.getUsername(), currentAnalyst.getId());
         }
 
-        List<FeedCardDTO> feedCardDTOS = createFeedCardDTOList(analystUsernameToIdMap, feedCards, zoneId);
+        List<FeedCardDTO> feedCardDTOS = createFeedCardDTOList(analystUsernameToIdMap, feedCards, timeZoneToUse.toZoneId());
         if (feedCards.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
